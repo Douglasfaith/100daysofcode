5bb5c5b0791c7c69424be4985b5b8159dbde405d
Douglasfaith <faith.okpongete@gmail.com>
2022-07-12 11:06:11 -0700

    edit helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,4 +3,34 @@
 #print ("name")
 #/n new line on the screen
 
-print ("Hello World faith")
+#thiscomment is for helloworld
+""" this is faith code
+"""
+#print ("Hello World faith")
+# from calendar import month
+# from unicodedata import name
+
+
+#this code will store the input from the user
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+
+#datatype coversion
+print (int(num1) + int(num2))
+
+#this will add the input collected - float datatype int - whole numberb 
+#sum = float(num1) + float(num2)
+
+#display the sum (formatting with placeholders) counting starts at 0
+#print("the sum of {0} and {1} is {2}". format (num1,num2,sum))
+
+#datetime library #using timedelta function to specify a period of time (add or remove days/weeks)
+from datetime import datetime, timedelta
+# current_date = datetime.now()
+# print('Today is:' +str(current_date)) #
+ 
+
+
+
+
+#errorhandling with date types
